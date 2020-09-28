3d0a20866fb89bd2b5429221978edf8caa924b15
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-28 04:48:33 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,12 +2,22 @@ require 'pry'
 
 class GetRequester
   
+  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+  
+  def initialize(site)
+    @site = site
+  end 
+  
   def get_response_body
-    
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body 
   end
   
   def parson_json
-    
+    programs = JSON.parse(get_response_body)
+    programs.collect do |program|
+      program["agency"]
   end
 
 end
